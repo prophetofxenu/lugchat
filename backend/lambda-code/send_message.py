931ad854220ac937f5a110a331d83b0644e8d3dc
William Harrell <wharrell1@protonmail.com>
2021-02-27 17:15:41 -0500

    implemented send message API endpoint

--- backend/lambda-code/send_message.py
+++ backend/lambda-code/send_message.py
@@ -1,5 +1,64 @@
 import boto3
+from datetime import datetime, timedelta
+import json
+import sys
+
+
+TABLE_NAME = 'LUGChat'
+
+
+dynamo = boto3.client('dynamodb')
+
+
+def push_to_db(chatroom, user, timestamp, message):
+    try:
+        expires_on = (timestamp + timedelta(days=1)).timestamp()
+        expires_on = str(expires_on).split('.')[0]
+        item = {
+            'Chatroom': {
+                'S': chatroom
+            },
+            'User': {
+                'S': user
+            },
+            'Timestamp': {
+                'S': str(timestamp)
+            },
+            'Message': {
+                'S': message
+            },
+            'ExpiresOn': {
+                'N': expires_on
+            }
+        }
+        dynamo.put_item(Item=item, TableName=TABLE_NAME)
+        return True, 'Message pushed to DB'
+    except Exception as e:
+        print('Unable to push message to dynamo', file=sys.stderr)
+        print(str(e), file=sys.stderr)
+        return False, 'Error pushing message to DB'
+
 
 def handler(event, context):
-    pass
+    params = event['pathParameters']
+    status_code = 200
+    res_body = {}
+    try:
+        chatroom = params['chatroom']
+        body = json.loads(event['body'])
+        user = body['user']
+        timestamp = datetime.now()
+        message = body['message']
+        ok, msg = push_to_db(chatroom, user, timestamp, message)
+        res_body['res'] = msg
+        if not ok:
+            status_code = 500
+    except Exception as e:
+        res_body['res'] = 'Generic error'
+        status_code = 500
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(res_body),
+        'isBase64Encoded': False
+    }
 
