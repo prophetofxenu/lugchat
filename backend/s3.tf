27d1dbd62c993db0076922623a8a64a8dc88d6fb
William Harrell <wharrell1@protonmail.com>
2021-03-13 16:16:24 -0500

    added randomness to S3 bucket name

--- backend/s3.tf
+++ backend/s3.tf
@@ -1,5 +1,9 @@
+resource "random_id" "bucket" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "lugchat" {
-  bucket = "lugchat-frontend"
+  bucket = "lugchat-frontend-${random_id.bucket.hex}"
   acl = "public-read"
 
   website {
